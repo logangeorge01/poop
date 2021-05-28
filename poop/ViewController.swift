0f584909a0e09b99bf0a7413788072fd1ab37509
Logan George <logangeorge01@gmail.com>
2021-05-27 21:13:41 -0500

    Initial Commit

---
+++ poop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  poop
+//
+//  Created by Logan George on 5/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
